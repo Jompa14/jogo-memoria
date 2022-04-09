821c9952ea45a4b317033fa6cede34d368918218
Jompa14 <joao.buchholz@gmail.com>
2022-04-09 15:01:12 -0300

    cria cards componente

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TabuleiroCartas/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabuleiroCartas from './components/TabuleiroCartas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TabuleiroCartas,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+        background-color: rgb(29, 4, 34);
+        color: rgb(254, 212, 62);
+    }
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
 </style>
