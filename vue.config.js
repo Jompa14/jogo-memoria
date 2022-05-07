82331b8d24450233ba0f73b33418e43224666dcc
Jompa14 <joao.buchholz@gmail.com>
2022-05-07 15:58:09 -0300

    remove vue connfig webpack

--- vue.config.js
+++ vue.config.js
@@ -4,16 +4,3 @@ module.exports = defineConfig({
     'vuetify'
   ]
 })
-const webpack = require('webpack')module.exports = {
-  configureWebpack: {
-    plugins: [
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 1
-      })
-    ]
-  },
-  chainWebpack:
-    config => {
-      config.optimization.delete('splitChunks')
-    }
-}
